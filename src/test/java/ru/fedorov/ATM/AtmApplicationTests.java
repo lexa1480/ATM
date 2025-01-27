79dae43ab4140c3196fbee93b651af5db86293c6
lexa1480 <lexa.f14@gmail.com>
2025-01-28 00:38:25 +0300

    Initial commit

---
+++ src/test/java/ru/fedorov/ATM/AtmApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fedorov.ATM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
