0e7c2dbd2f6622e294e423b1730a4c18c20d501e
lexa1480 <lexa.f14@gmail.com>
2025-02-01 17:17:09 +0300

    Swagger - added

--- src/main/java/ru/fedorov/ATM/controllers/ATMController.java
+++ src/main/java/ru/fedorov/ATM/controllers/ATMController.java
@@ -1,19 +1,33 @@
 package ru.fedorov.ATM.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.fedorov.ATM.dto.Cash;
+import ru.fedorov.ATM.services.CashService;
 
+@Schema(description = "Контроллер банкомата")
 @RestController
 @RequestMapping("/ATM")
 public class ATMController {
 
-        @GetMapping("/cash")
-        public Cash getCash()
-        {
-            Cash cash = new Cash();
+    private final CashService cashService;
 
-            return cash;
-        }
+
+    @Autowired
+    public ATMController(CashService cashService) {
+        this.cashService = cashService;
+    }
+
+    @Operation(summary = "Покажет сумму денег")
+    @GetMapping("/cash")
+    public Cash getCash()
+    {
+        Cash cash = new Cash();
+
+        return cash;
+    }
 }
--- src/main/java/ru/fedorov/ATM/services/CashService.java
+++ src/main/java/ru/fedorov/ATM/services/CashService.java
@@ -1,4 +1,11 @@
 package ru.fedorov.ATM.services;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Schema(description = "Бизнес логика банкомата")
+@Service
+@Transactional
 public class CashService {
 }
