fab6a00a8bf6ef910dd8d046bdd1345880174aab
lexa1480 <lexa.f14@gmail.com>
2025-02-06 00:08:05 +0300

    some work AtmController

--- src/main/java/ru/fedorov/ATM/controllers/ATMController.java
+++ src/main/java/ru/fedorov/ATM/controllers/ATMController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.fedorov.ATM.dto.CashDTO;
@@ -25,6 +26,15 @@ public class ATMController {
     @Operation(summary = "Покажет сумму денег")
     @GetMapping("/cash")
     public CashDTO getCash()
+    {
+        CashDTO cashDTO = new CashDTO();
+
+        return cashDTO;
+    }
+
+    @Operation(summary = "Покажет сумму денег")
+    @PostMapping("/cash")
+    public CashDTO setCash()
     {
         CashDTO cashDTO = new CashDTO();
 
